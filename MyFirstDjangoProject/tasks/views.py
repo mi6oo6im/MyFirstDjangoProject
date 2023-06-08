08a5939dd475eb9137cb612bba09e24385f35771
Mihail Istiliyanov <mi6oo6im@github.com>
2023-06-08 19:54:59 +0100

    add template

--- MyFirstDjangoProject/tasks/views.py
+++ MyFirstDjangoProject/tasks/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from MyFirstDjangoProject.tasks.models import Task
 
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("It works!")
 
 
 def all_tasks(request):
-    return HttpResponse("It works!")
+    result = Task.objects.all()
+    context = {
+        "title": "Today's tasks",
+        "tasks": result,
+    }
+    return render(request, "tasks/all_tasks.html", context)
