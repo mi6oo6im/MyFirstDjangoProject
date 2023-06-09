0261e4305077a0919fe788c3b474349dd66d7741
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2023-06-09 14:04:30 +0100

    Update settings.py

--- MyFirstDjangoProject/settings.py
+++ MyFirstDjangoProject/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "web_tasks",
         "USER": "mi6oo6im",
-        "PASSWORD": "Parola1@",
+        "PASSWORD": "********",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
